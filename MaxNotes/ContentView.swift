73cde90c5db9913454ef46a68e9728d5eaab3654
mxzmr <102792047+mxzmr@users.noreply.github.com>
2025-11-23 17:42:45 +0200

    Initial Commit

---
+++ MaxNotes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MaxNotes
+//
+//  Created by Max zam on 23/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MaxNotes/MaxNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaxNotesApp.swift
+//  MaxNotes
+//
+//  Created by Max zam on 23/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct MaxNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
